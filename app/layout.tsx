bedc9e818d875a0f9a7e351cdcd7140938603664
dewer-env <dewer.env@gmail.com>
2026-06-23 18:29:24 -0300

    muda slug

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'AGENDAMENTO - capelamilagres',
+  title: 'CALENDÁRIO - capelamilagres',
   description: 'Reserve seu espaço na Capela dos Milagres',
   icons: { icon: '/favicon.png' },
 }
